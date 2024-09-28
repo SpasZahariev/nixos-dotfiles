adb551d1d387dec3b1184ac8c4aef3aa4adb4bab
spas.z <spaszahariev54@gmail.com>
2024-09-28 08:40:55 +0000

    adding some scripts for openning apps

--- .config/hypr/scripts/select-and-paste.sh
+++ .config/hypr/scripts/select-and-paste.sh
@@ -8,11 +8,28 @@
 # didn't work
 # copyq show; wtype $(copyq read 0)
 
+
+clear_duplicates() {
+    copyq eval "
+    var seen = {};
+    for (var i = size() - 1; i >= 0; --i) {
+        var item = str(read(i));
+        if (seen[item]) {
+            remove(i);
+        } else {
+            seen[item] = true;
+        }
+    }
+    ";
+}
+
+
 # Check if CopyQ is visible
 if copyq visible | rg -q "true"; then
     # If visible, hide it
     copyq hide
 else
+    clear_duplicates
     # If not visible, show it
     copyq show
 fi
---
+++ .config/hypr/scripts/spotify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Command to start spotify
+# spotify_command="setsid spotify-launcher --skip-update &"
+
+# $spotify_command
+setsid spotify-launcher --skip-update &
+# disown
+
+# exit 0
