3080952c16350a1189d9a9b5f75ae9a19a46afc3
spas.z <spaszahariev54@gmail.com>
2024-09-29 12:11:49 +0000

    open work script is working nicely

--- .config/hypr/scripts/open-work.sh
+++ .config/hypr/scripts/open-work.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Command to create or attach to the 'work' tmux session and start Vim
-tmux_command="tmux new-session -A -s work -c ~ 'nvim .'"
+tmux_command="cd ~/PROGRAMMING/ ; tmux new-session -A -s work"
 
 # Open Alacritty and execute the tmux command
 alacritty -e bash -c "$tmux_command"
