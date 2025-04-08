5cb07673f937b4229e1cbcfd70bb2e30c02c3b3b
Spas Zahariev <spaszahariev54@gmail.com>
2025-04-08 18:00:11 +0200

    fix spotify launch script

--- .config/hypr/scripts/rand_wallpaper_middle.sh
+++ .config/hypr/scripts/rand_wallpaper_middle.sh
@@ -1,4 +1,4 @@
-# #!/usr/bin/env bash
+#!/usr/bin/env bash
 
 # === Config ===
 WIDE_WALLPAPER_DIR="$HOME/dotfiles/wallpapers/wide"
--- .config/hypr/scripts/spotify.sh
+++ .config/hypr/scripts/spotify.sh
@@ -1,10 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Command to start spotify
 # spotify_command="setsid spotify-launcher --skip-update &"
 
 # $spotify_command
-setsid spotify-launcher --skip-update &
+setsid spotify --skip-update &
+# spotify --skip-update &
 # disown
 
 # exit 0
