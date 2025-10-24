ebf6a20df10a693c49b817b5e79f7ddfce61466c
Spas Zahariev <spaszahariev54@gmail.com>
2025-10-24 23:58:12 +0200

    adding treesitter config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,19 @@
-if true then
-  return {}
-end
--- return {
---   {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     lazy = true,
---     opts = {
---       flavour = "macchiato",
---     },
---   },
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "catppuccin",
---     },
---   },
--- }
---
+-- if true then
+--   return {}
+-- end
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    opts = {
+      flavour = "macchiato",
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
