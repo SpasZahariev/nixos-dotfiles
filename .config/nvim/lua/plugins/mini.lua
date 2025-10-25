e32363780e3f9f4f9220e7908480b9706e9129d6
Spas Zahariev <spaszahariev54@gmail.com>
2025-10-25 16:07:50 +0200

    adding some mini pluggins

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "echasnovski/mini.ai", -- adds moveminets Inside and Around stuff
+    version = false, -- latest unstable changes here we go!
+    event = "VeryLazy", -- neovim thing. Will load the plugin after everything is ready
+    config = function()
+      require("mini.ai").setup({
+        n_lines = 500, -- how many lines above and bellow mini.ai will search for
+      })
+    end,
+  },
+
+  {
+    "echasnovski/mini.surround", -- better surround than tpopes
+    version = false,
+    event = "VeryLazy",
+    config = function()
+      require("mini.surround").setup()
+    end,
+  },
+
+  {
+    "echasnovski/mini.files",
+    version = false,
+    event = "VeryLazy",
+    config = function()
+      local mini_files = require("mini.files")
+      mini_files.setup({
+        windows = {
+          preview = true, -- show file preview on the right
+          width_focus = 30,
+          width_preview = 60,
+        },
+        -- options = {
+        --   use_as_default_explorer = true,
+        -- },
+      })
+
+      -- Keymap to toggle MiniFiles in the current directory
+      vim.keymap.set("n", "<leader>E", function()
+        require("mini.files").open(vim.api.nvim_buf_get_name(0))
+      end, { desc = "Open Mini Files (file explorer)" })
+    end,
+  },
+}
