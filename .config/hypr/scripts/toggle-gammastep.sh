6a8f8b7b47607fa93373bf6a7540e77c587cd44d
Spas Zahariev <spaszahariev54@gmail.com>
2025-10-25 21:08:14 +0200

    tweaking gamma script

--- .config/hypr/scripts/toggle-gammastep.sh
+++ .config/hypr/scripts/toggle-gammastep.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 # Check if gammastep is running
-if pgrep -x "gammastep" >/dev/null; then
+count=$(ps | rg gammastep | wc -l)
+if [ "$count" -gt 0 ]; then
   echo "Gammastep is running — stopping it..."
-  pkill -x gammastep
+  pkill gammastep
 else
   echo "Gammastep is not running — starting it for Zurich..."
-  setsid gammastep -l 47.3769:8.5417 -t 5500:3500 &
+  setsid gammastep -m wayland -l 47.3769:8.5417 -t 5500:3500 &
 fi
