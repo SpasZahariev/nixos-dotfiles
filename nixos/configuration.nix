71b474b1c350f8c3823c489ec088202eea9d725a
Spas Zahariev <spaszahariev54@gmail.com>
2026-05-28 12:51:53 +0200

    making openwhiser start manually

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -321,8 +321,9 @@
 
       openwhispr = {
         description = "OpenWhispr voice transcription";
-        wantedBy = [ "default.target" ];
-        after = [ "default.target" ];
+        # Commented out wantedBy - i want to start this manually for now
+        # wantedBy = [ "default.target" ];
+        # after = [ "default.target" ];
 
         serviceConfig = {
           Type = "simple";
@@ -339,10 +340,10 @@
 
         serviceConfig = {
           Type = "simple";
-          ExecStart = ''
-            ${unstablePkgs.llama-cpp-vulkan}/bin/llama-server -hf unsloth/Qwen3.6-27B-MTP-GGUF:UD-Q4_K_XL --port 11434 --api-key "sk-local-token" -ngl 99 --flash-attn on --cache-type-k q8_0 --cache-type-v q8_0 -c 115344 -t 16 -b 2048 --mlock --temp 1.0 --top_p 0.95 --top_k 20 --presence_penalty 1.5 --spec-type draft-mtp --spec-draft-n-max 1'';
           # ExecStart = ''
-          #   ${unstablePkgs.llama-cpp-vulkan}/bin/llama-server -hf unsloth/Qwen3.6-27B-GGUF:UD-Q4_K_XL --port 11434 --api-key "sk-local-token" -ngl 99 --flash-attn on --cache-type-k q8_0 --cache-type-v q8_0 -c 115344 -t 16 -b 2048 --mlock --temp 1.0 --top_p 0.95 --top_k 20 --presence_penalty 1.5'';
+          #   ${unstablePkgs.llama-cpp-vulkan}/bin/llama-server -hf unsloth/Qwen3.6-27B-MTP-GGUF:UD-Q4_K_XL --port 11434 --api-key "sk-local-token" -ngl 99 --flash-attn on --cache-type-k q8_0 --cache-type-v q8_0 -c 115344 -t 16 -b 2048 --mlock --temp 1.0 --top_p 0.95 --top_k 20 --presence_penalty 1.5 --spec-type draft-mtp --spec-draft-n-max 1'';
+          ExecStart = ''
+            ${unstablePkgs.llama-cpp-vulkan}/bin/llama-server -hf unsloth/Qwen3.6-27B-GGUF:UD-Q4_K_XL --port 11434 --api-key "sk-local-token" -ngl 99 --flash-attn on --cache-type-k q8_0 --cache-type-v q8_0 -c 115344 -t 16 -b 2048 --mlock --temp 1.0 --top_p 0.95 --top_k 20 --presence_penalty 1.5'';
           Restart = "on-failure";
           RestartSec = "5s";
         };
